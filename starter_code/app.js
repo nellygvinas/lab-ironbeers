7aab7560ee870aade1730e2dcf92a8984bd24f9e
Nelly Gonzalez-Vinas <nramos4510@gmail.com>
2019-08-05 16:07:20 -0400

    Beers and random beers done, working on finishing display to make beers display nicely.

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,34 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 
-
-app.get('/', (req, res, next) => {
-  res.render('index');
+app.get('/', (req, res, next) => res.render('index'));
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(responseFromAPI => {
+    // console.log(responseFromAPI)
+    res.render('beers.hbs', {beers: responseFromAPI })
+  })
+  .catch(error => console.log(error))
 });
 
+app.get('/random-beer', (req, res, next) => {
+  const randomBeer = punkAPI.getRandom()
+    randomBeer.then(beer => {
+      res.render('random-beer.hbs', {beer}) 
+    })
+    .catch(error => {
+      console.log(error)
+    })  
+});
+
+app.get('/randombeer', (req, res, next) => res.render('random-beer'));
 
 
+
+
+
+// Set up server connection
 app.listen(3000);
